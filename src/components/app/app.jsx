248fc39de0d2894ee9e9157c40f18ee0136080c0
VadimKorobec <_s_e_t__@ukr.net>
2024-04-01 18:41:13 +0300

    add some components

---
+++ src/components/app/app.jsx
@@ -0,0 +1,23 @@
+import AppFilter from "../app-filter/app-filter";
+import AppInfo from "../app-info/app-info";
+import EmployessAddForm from "../employees-add-form/employees-add-form";
+import EmployeesList from "../employees-list/empoloyees-list";
+import SearchPanel from "../search-panel/search-panel";
+
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <AppInfo />
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+      <EmployeesList />
+      <EmployessAddForm />
+    </div>
+  );
+}
+
+export default App;
