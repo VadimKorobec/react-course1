4f3bbfa8ca099153a8a56be6a4a289959efc4f74
VadimKorobec <_s_e_t__@ukr.net>
2024-04-02 14:44:37 +0300

    add map

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,12 @@ import SearchPanel from "../search-panel/search-panel";
 import "./app.css";
 
 function App() {
+  const data = [
+    { name: "John", surname: "Smith", salary: 1000, increase: false },
+    { name: "Tom", surname: "Poor", salary: 800, increase: true },
+    { name: "Oleg", surname: "Hithand", salary: 290, increase: false },
+  ];
+
   return (
     <div className="app">
       <AppInfo />
@@ -14,7 +20,7 @@ function App() {
         <SearchPanel />
         <AppFilter />
       </div>
-      <EmployeesList />
+      <EmployeesList data={data} />
       <EmployessAddForm />
     </div>
   );
